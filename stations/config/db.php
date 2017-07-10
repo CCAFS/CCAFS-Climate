afbd7443f2ecaa8eaa029f381c33f34bb95e8eeb
jheduart <jaime.tm8@gmail.com>
2017-07-10 16:00:25 -0500

    add files to gitgnore

--- stations/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-  define("PG_DB"  , "ccafs_climate");
-  // define("PG_DB"  , "stations");
-  define("PG_HOST", "localhost"); 
-  define("PG_USER", "jetarapues");
-  define("PG_PASS", "ciat2016");
-  define("PG_PORT", "5432"); 
-  define("SRID",   "4326"); 
-?>
